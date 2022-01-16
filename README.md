04719d71d4372ad06e176fb78287561502d35942
MakWils <kaylaanngrace@gmail.comgit remote add origin git@github.com:kaylaanngrace/horiseon-optimize-accessibility.git>
2022-01-15 19:14:16 -0600

    ensure one character option is picked

--- README.md
+++ README.md
@@ -13,7 +13,14 @@ My motive for this project is to become more efficent in web development/coding.
 
 
 ## CREDITS:
+psuedo code for script.js file - https://youtu.be/x4HUaiazDes
 
+Make whole array upperCase - https://attacomsian.com/blog/javascript-array-lowercase-uppercase
+
+Password special characters - https://www.owasp.org/index.php/Password_special_characters
+
+merge arrays https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
+copy to clipboard - https://www.w3schools.com/howto/howto_js_copy_clipboard.asp
 
 ## CONTRIBUTIONS
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,55 @@
 // Assignment code here
-// var confirmLength;
-// var confirmNumerics;
-// var confirmUppercase;
-// var confirmLowercase;
-// var confirmSpecialCharacter;
+
+// confirm variables:
+var confirmLength=""
+var confirmNumerics;
+var confirmUppercase;
+var confirmLowercase;
+var confirmSpecialCharacter;
+var pwdCharacters
 
 // variable values:
 // numeric characters
 var numerics = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(numerics)
 
 // lowerCase characters
 var alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-console.log(alpha)
 
 // upperCase characters
 var upperCase = alpha.map(alpha => alpha.toUpperCase());
-console.log(upperCase);
 
 // special characters 
 var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-console.log(specialChar)
 
 // generatePassword function
 function generatePassword() {
   console.log("you clicked the button");
   // Step 1 prompt the user for the password criteria
-  length();
-  if (length) {
-
-  }
-
-  // Step 2 validate input, ensure at least one character type is choosen. 
-
-  // Step 3 Generate password based on inputs 
-
-  // Step 4 display password to page.
-
-  var randomPassword = "this is your secure password";
-  // return variable
-  return randomPassword;
-}
-
-
-// pick password length btwn 8-128 characters
-var length = function() {
+  // pick length
   var confirmLength = window.prompt("How long would you like your password? Please choose a number between 8 and 128");
   confirmLength = parseInt(confirmLength);
-  console.log(confirmLength);
-  
+
   if (!confirmLength) {
     alert("Please enter a number value.");
-    return length(); 
+    generatePassword();
   } else if (confirmLength < 8 || confirmLength > 128) {
-    alert ("Password length MUST be between 8 and 128 characters.")
-    return length();
-  }
-};
-
-// confirm to include or not, lowercase, uppercase, numeric, and/or special characters 
+    alert ("Password length MUST be between 8 and 128 characters.");
+    generatePassword();
+  } else {
+    // confirm to include or not, lowercase, uppercase, numeric, and/or special characters 
+    confirmLowercase = window.confirm("Would you like to include lowercase letter? For yes, Click ok. For no, click cancel.");
+    confirmUppercase = window.confirm("Would you like to include Uppercase letter? For yes, Click ok. For no, click cancel.");
+    confirmNumerics = window.confirm("Would you like to include numbers? For yes, Click ok. For no, click cancel.");
+    confirmSpecialCharacter = window.confirm("Would you like to include special characters? For yes, Click ok. For no, click cancel.");
+  };
  
+  // Step 2 validate input, ensure at least one character type is choosen. 
+  if (!confirmLowercase && !confirmUppercase && !confirmNumerics && !confirmSpecialCharacter) {
+    characters = alert("MUST pick at least ONE character option.");
+    generatePassword ();
+  } 
+
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -72,15 +62,17 @@ function writePassword() {
   passwordText.value = password;
 };
 
+// copy button element 
 var copyDiv = document.createElement("div");
 var copyBtn = document.createElement("button");
 copyBtn.className = "btn";
 copyBtn.id = "copy";
 copyBtn.textContent = "COPY";
-
+// appendChild to display on page
 document.getElementById("buttons").appendChild(copyDiv)
 copyDiv.appendChild(copyBtn)
 
+// copy to clipboard function
 function copy() {
   var copyText = document.querySelector("#password");
   copyText.select();
@@ -89,6 +81,5 @@ function copy() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+// Add event listerner to copy button
 copyBtn.addEventListener("click", copy);
- 
-
